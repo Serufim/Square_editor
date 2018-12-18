319719cc621021a322cc3037e90f5c944f085213
serufim <serufim97@yandex.ru>
2018-12-19 00:21:22 +0800

    Почти готово, осталось только сделать выделение квадратика

--- script.js
+++ script.js
@@ -1,24 +1,57 @@
 class Square {
-    constructor(data, centerPosition) {
-        this.width = data.width ? data.width : this.getRandom("width");
-        this.height = data.height ? data.height : this.getRandom("height");
-        this.color = data.color ? data.color : this.getRandom("color");
-        this.centerPosition = centerPosition;
-        this.leftTopPosition = [centerPosition[0] - this.width / 2, centerPosition[1] - this.height / 2];
+    constructor(data, leftTopPosition) {
+        this.width = data.width;
+        this.color = data.color;
+        this.centerPosition = [leftTopPosition[0]-this.width/2,leftTopPosition[1]-this.width/2];
+        this.leftTopPosition = leftTopPosition;
+        this.selected = true
+    }
+    selectSquare(){
+        this.selected = true
     }
 }
 
 class Editor {
     constructor() {
         this.squares = [];
-        const canvas = document.getElementById("editor");
-        canvas.addEventListener('click', e => this.createSquare(e));
-        this.ctx = canvas.getContext('2d');
+        this.canvas = document.getElementById("editor");
+        this.canvas.addEventListener('click', e => this.createSquare(e));
+        this.ctx = this.canvas.getContext('2d');
         this.drawTestArc()
     }
-
+    getRandomColor()
+    {
+        var color = "";
+        for(var i = 0; i < 3; i++) {
+            var sub = Math.floor(Math.random() * 256).toString(16);
+            color += (sub.length == 1 ? "0" + sub : sub);
+        }
+        return "#" + color;
+    }
+    getCursorPosition(event) {
+        const {canvas} = this;
+        var rect = canvas.getBoundingClientRect();
+        var x = event.clientX - rect.left;
+        var y = event.clientY - rect.top;
+        return [x,y];
+    }
     createSquare(e) {
         //Забираем значения из форм
+        let width = document.getElementById("square-width").value;
+        let color = document.getElementById("square-color").value;
+        if (!width){
+            width = Math.floor(Math.random() * (40 - 10 + 1)) + 10;
+            color = this.getRandomColor();
+        }
+        //Рисуем квадрат
+        const cords = this.getCursorPosition(e);
+        const newSquare = new Square({
+            width: width,
+            color: color
+        }, cords);
+        console.log(color);
+        this.squares.push(newSquare);
+        this.drawSquares();
     }
 
     drawTestArc() {
@@ -31,7 +64,12 @@ class Editor {
     drawSquares() {
         const {ctx} = this;
         for (let square of this.squares) {
-            ctx.fillRect(square.leftTopPosition[0], square.leftTopPosition[0], square.width, square.height);
+            ctx.fillStyle=square.color;
+            ctx.fillRect(square.centerPosition[0],square.centerPosition[1], square.width, square.width);
+            if(square.selected){
+                ctx.strokeStyle = "#000000";
+                ctx.strokeRect(square.centerPosition[0]-1,square.centerPosition[1]-1, square.width, square.width);
+            }
         }
     }
 }
